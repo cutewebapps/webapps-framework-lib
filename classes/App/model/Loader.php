4f2ea22e86bbc205bbc04e642ddf7a9c2e5beca9
Victor Lopata <v.lopata@webcerebrium.com>
2013-04-12 13:43:35 -0400

    function (partially) dump web database easily

--- classes/App/model/Loader.php
+++ classes/App/model/Loader.php
@@ -54,11 +54,126 @@ function cwa_init_web_application()
     }
     App_Application::getInstance()->connectDb();
 }
+
 function cwa_run_web_application()
 {
     // run routers - path to all application controllers
     App_Application::getInstance()->run();
 }
+
+
+function cwa_dump_web_application( $arrTables = array(), $strConnection = 'default' )
+{
+    $objConnectionConfig = App_Application::getInstance()->getConfig()->connections->$strConnection;
+    $arrParams = $objConnectionConfig->params->toArray();
+
+    App_CheckEnv::assert(  isset( $arrParams['host'] ), 'No host defined for "'.$strConnection.'" connection' );
+    App_CheckEnv::assert(  isset( $arrParams['username'] ), 'No username defined for "'.$strConnection.'" connection' );
+    App_CheckEnv::assert(  isset( $arrParams['dbname'] ), 'No database defined for "'.$strConnection.'" connection' );
+    $strHost     = $arrParams['host'];
+    $strUsername = $arrParams['username'];
+    $strPassword = isset( $arrParams['password'] ) ? $arrParams['password'] : '' ;
+    $strDatabase = $arrParams['dbname'];
+    $strCharset  = isset( $arrParams['charset'] ) ? $arrParams['charset'] : '' ;
+
+    App_CheckEnv::assert( mysql_connect( $strHost, $strUsername, $strPassword ),
+            "no mysql connection for ".$strConnection );
+    App_CheckEnv::assert( mysql_selectdb( $strDatabase ),
+            "mysql database cannot be selected for ".$strConnection );
+    
+    App_CheckEnv::assert( CWA_APPLICATION_DIR.'/cdn', 
+            "CDN folder is not found for output" );
+
+    // if we dont know the list of tables, lets get it
+    $rs = mysql_query('SHOW TABLE STATUS FROM ' . $strDatabase ) or die( mysql_error() );
+    $arrAllTables = array();
+    while( $rs && $r = mysql_fetch_array( $rs )) {
+        $arrAllTables[ $r['Name'] ]  = $r['Name'];
+    }
+        
+        
+    if ( count( $arrTables ) != 0 ) {
+        // verify each table
+        foreach ( $arrTables as $strTable ) {
+            if ( !isset( $arrAllTables[ $strTable ] ))
+                throw new Exception( $strTable.' table not found in the database' );
+        }
+    } else {
+        $arrTables = $arrAllTables;
+    }
+    
+    $out = '';
+    if ( $strCharset != '' ) $out .= 'SET NAMES '.$strCharset.";\n";
+    
+    foreach(  $arrTables as $strTable ) {
+        Sys_Io::out( $strTable );
+        
+        $rst = mysql_query("SHOW CREATE TABLE `$strTable`") or die( mysql_error() );
+        $fi = array();
+        if ($rst && $row2 = mysql_fetch_row($rst)) {
+            $o = "DROP TABLE IF EXISTS `$strTable`;\n" . $row2[1] . ";\n\n";
+            // echo $o;
+            $fields = explode("\n", $row2[1]);
+            // dbg( $fields );
+            foreach ($fields as $i => $fld) {
+                if (preg_match("/^\s*`([^`]+)`(.+)$/", $fld, $m)) {
+                    $fi[$m[1]] = $m[2];
+                }
+            }
+            $out .= $o;
+        }
+        // for each table record..
+        $rsr = mysql_query("SELECT * FROM `$strTable`") or die( mysql_error() );
+        while ($rsr && $row2 = mysql_fetch_array($rsr)) {
+            $o = "\r\nINSERT INTO " . $strTable . " (";
+            $col = 0;
+            foreach ($row2 as $column => $value) {
+                if (substr($column, 0) != substr(intval($column), 0)) {
+                    if ($col != 0)
+                        $o .= ",";
+                    $o .= '`'."$column".'`';
+                    $col ++;
+                }
+            }
+            $o .= ")\r\n\tVALUES (";
+            $col = 0;
+            foreach ($row2 as $column => $value) {
+                if (substr($column, 0) != substr(intval($column), 0)) {
+                    if ($col != 0)
+                        $o .= ",";
+                    $v = "";
+                    if ($value == "") {
+                        $v = "NULL";
+                        if (strstr($fi[$column], "NOT NULL"))
+                            $v = "''";
+                    } else {
+                        $v = "'" . str_replace("\\", "\\\\", str_replace("'", "''", $value)) . "'";
+                    }
+                    $o .= $v;
+                    $col ++;
+                }
+            }
+            $o .= ");\r\n";
+            $out .= $o;
+        }
+    }
+    
+    
+    $fn = trim(strtolower( $strDatabase )) . "_" . date("YmdHis") . ".zip";
+    $z = new Sys_ZipFile();
+    $z->add_file($out, trim(strtolower( $strDatabase )) . "_" . date("Ymd") . ".sql");
+    $f = fopen( CWA_APPLICATION_DIR."/cdn/".$fn, "wb");
+    if ($f) {
+        fwrite($f, $z->file());
+        fclose($f);
+        if ( file_exists( $fn )) { chmod( $fn, 0666); }
+        echo "<br />Please, download <a href='./cdn/$fn'>$fn</a> ("  . filesize( CWA_APPLICATION_DIR."/cdn/".$fn ) . " bytes) ";
+    } else {
+        echo 'Sorry, file was not created ';
+    }
+}
+
+
 function cwa_patch_web_application() 
 {
     // get all namespaces
@@ -70,6 +185,7 @@ function cwa_patch_web_application()
     DBx_Table_Abstract::setDefaultMetadataCache( null );
     App_Update::run( $arrNamespaces );
 }
+
 /**
  * Routine for checking web applications environment
  * Can be put as a separate script 

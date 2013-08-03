050a8cc589dc6e9ae802bcba29c91374b14173cc
Victor Lopata <webmaster@cutewebapps.com>
2013-08-03 05:19:04 -0400

    Xhprof

---
+++ classes/App/ctrl/plugin/Xhprof.php
@@ -0,0 +1,31 @@
+<?php
+
+class App_Xhprof_CtrlPlugin extends App_Dispatcher_CtrlPlugin
+{
+ 
+    public function preDispatch()
+    {
+        $bEnabled = App_Application::getInstance()->config->xhprof;
+        if ( $bEnabled ) {
+            ini_set( 'xhprof.output_dir', '' );
+            xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+        }
+        
+        return true;
+    }
+
+    public function postDispatch()
+    {
+        $bEnabled = App_Application::getInstance()->config->xhprof;
+        if ( $bEnabled ) {
+            $xhprof_data = xhprof_disable();
+//            $XHPROF_ROOT = "/tools/xhprof/";
+//            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+//            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+//            $xhprof_runs = new XHProfRuns_Default();
+//            $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
+//            echo "http://localhost/xhprof/xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
+        }
+        return true;
+    }
+}

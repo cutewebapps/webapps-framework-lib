a9fbb7e78701aa7f6c989ac1409a98e0894a5f38
Victor Lopata <webmaster@cutewebapps.com>
2013-08-03 05:50:41 -0400

    xhProf - saving debugging history

--- classes/App/ctrl/plugin/Xhprof.php
+++ classes/App/ctrl/plugin/Xhprof.php
@@ -16,12 +16,12 @@ class App_Xhprof_CtrlPlugin extends App_Dispatcher_CtrlPlugin
 
     public function postDispatch()
     {
-        $bEnabled = App_Application::getInstance()->config->xhprof;
+        $bEnabled = App_Application::getInstance()->getConfig()->xhprof;
         if ( $bEnabled ) {
             $xhprof_data = xhprof_disable();
             
             $strId = date('His').'.'.mt_rand(10000,99999);
-            $dir = new Sys_Dir( App_Application::getInstance()->config->xhprof.'/'.date('Ymd') );
+            $dir = new Sys_Dir( App_Application::getInstance()->getConfig()->xhprof.'/'.date('Ymd') );
             if ( !$dir->exists() ) $dir->create( '', true );
                 
             $debugFile = new Sys_File( $dir.'/'.$strId.'.txt' );
@@ -32,13 +32,6 @@ class App_Xhprof_CtrlPlugin extends App_Dispatcher_CtrlPlugin
                 $strOut .= implode( "", $arrColumns ) . "\n";
             }
             $debugFile->save( $strOut );
-            
-//            $XHPROF_ROOT = "/tools/xhprof/";
-//            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
-//            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
-//            $xhprof_runs = new XHProfRuns_Default();
-//            $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
-//            echo "http://localhost/xhprof/xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
         }
         return true;
     }

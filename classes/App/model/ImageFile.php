d1e3569ae9a2f20c805131c6e18a41395403438b
Victor Lopata <webmaster@cutewebapps.com>
2013-11-27 06:21:10 -0500

    generateThumnbailCrop

--- classes/App/model/ImageFile.php
+++ classes/App/model/ImageFile.php
@@ -536,4 +536,49 @@ class App_ImageFile {
 
         return ($res)? $imgResult : null;
     }
+    
+    function generateThumbnailCrop( $file_output, $crop = 'square', $percent = false) {
+	list($w_i, $h_i, $type) = getimagesize( $this->getFilePath() );
+	if (!$w_i || !$h_i) {
+            throw App_Exception( 'Unable to get the length and width of the image' );
+            return;
+        }
+        $types = array('','gif','jpeg','png');
+        $ext = $types[$type];
+        if ($ext) {
+            $func = 'imagecreatefrom'.$ext;
+            $img = $func($this->getFilePath());
+        } else {
+            echo 'Invalid file format';
+            return;
+        }
+	if ($crop == 'square') {
+            $min = $w_i;
+            if ($w_i > $h_i) $min = $h_i;
+            $w_o = $h_o = $min;
+	} else {
+            list($x_o, $y_o, $w_o, $h_o) = $crop;
+            if ($percent) {
+                    $w_o *= $w_i / 100;
+                    $h_o *= $h_i / 100;
+                    $x_o *= $w_i / 100;
+                    $y_o *= $h_i / 100;
+            }
+    	if ($w_o < 0) $w_o += $w_i;
+	    $w_o -= $x_o;
+            if ($h_o < 0) $h_o += $h_i;
+            $h_o -= $y_o;
+	}
+	$img_o = imagecreatetruecolor($w_o, $h_o);
+	imagecopy($img_o, $img, 0, 0, $x_o, $y_o, $w_o, $h_o);
+        if ( file_exists( CWA_APPLICATION_DIR . $file_output) )
+            unlink ( CWA_APPLICATION_DIR . $file_output);
+	if ($type == 2) {
+            return imagejpeg($img_o, CWA_APPLICATION_DIR . $file_output,100);
+	} else {
+            $func = 'image'.$ext;
+            return $func($img_o, CWA_APPLICATION_DIR . $file_output);
+	}
+        imagedestroy($img_o);
+    }
 }

cb9836b877c105dbaf05a49b74a056227d61731b
Victor Lopata <webmaster@cutewebapps.com>
2014-04-11 09:21:00 -0400

    HeadTitle: h2 h3

--- classes/App/helper/HeadTitle.php
+++ classes/App/helper/HeadTitle.php
@@ -29,6 +29,7 @@ class App_HeadTitleHelper extends App_ViewHelper_Abstract
     {
         return self::getInstance();
     }
+    
     /**
      * @return App_HeadTitleHelper
      */
@@ -38,17 +39,16 @@ class App_HeadTitleHelper extends App_ViewHelper_Abstract
         return $this;
     }
     
+    /**
+     * @return boolean
+     */
     public function hasTitle()
     {
-        if ( $this->_strTitle == '' ) {
-            return FALSE;
-        } else {
-            return TRUE;
-        }
+        return ( $this->_strTitle != '' );
     }
 
     /**
-     * Output of the title as class
+     * Output of the title as H1-tag
      * @return App_HeadTitleHelper
      */
     public function h1( $strTitle = '' )
@@ -56,6 +56,31 @@ class App_HeadTitleHelper extends App_ViewHelper_Abstract
         echo '<h1>'. (( $strTitle != '' ) ? $strTitle : $this->_strTitle) .'</h1>'."\n";
         return $this;
     }
+    
+    /**
+     * Output of the title as h2 tag (useful with some css frameworks)
+     * @return App_HeadTitleHelper
+     */
+    public function h2( $strTitle = '' )
+    {
+        echo '<h2>'. (( $strTitle != '' ) ? $strTitle : $this->_strTitle) .'</h2>'."\n";
+        return $this;
+    }
+    
+    /**
+     * Output of the title as h2 tag (useful  with some css frameworks)
+     * @return App_HeadTitleHelper
+     */
+    public function h3( $strTitle = '' )
+    {
+        echo '<h2>'. (( $strTitle != '' ) ? $strTitle : $this->_strTitle) .'</h3>'."\n";
+        return $this;
+    }
+    
+    /**
+     * Get helper output in HEAD-tead
+     * @return string
+     */
     public function get()
     {
         return "\n".'<title>'.htmlspecialchars( $this->_strTitle, ENT_QUOTES ).'</title>';
